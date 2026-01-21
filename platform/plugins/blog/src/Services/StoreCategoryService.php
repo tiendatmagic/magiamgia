6aed23df6004aad2185cc5ce44a80dfaf06eef4a
tiendatmagic <tiendatmagic8@gmail.com>
2026-01-21 15:26:18 +0700

    update set default category

--- platform/plugins/blog/src/Services/StoreCategoryService.php
+++ platform/plugins/blog/src/Services/StoreCategoryService.php
@@ -10,6 +10,17 @@ class StoreCategoryService extends StoreCategoryServiceAbstract
 {
     public function execute(Request $request, Post $post): void
     {
-        $post->categories()->sync($request->input('categories', []));
+        $categories = $request->input('categories', []);
+
+        if (empty($categories)) {
+            if (class_exists(\Botble\Blog\Models\Category::class)) {
+                $default = \Botble\Blog\Models\Category::query()->where('is_default', 1)->first();
+                if ($default) {
+                    $categories = [$default->id];
+                }
+            }
+        }
+
+        $post->categories()->sync($categories);
     }
 }
